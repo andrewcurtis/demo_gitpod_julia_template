6c698e883ca4c23f961502110146e939c5df8ff6
Andrew Curtis <andrew.curtis@gmail.com>
2022-01-02 16:51:29 +0000

    I made some edits via gitpod online!

--- hello_world.jl
+++ hello_world.jl
@@ -1,1 +1,29 @@
+##
 println("hello world")
+##
+function test(x)
+    x + 3.0
+end
+
+## 
+test(123)
+
+##
+function greeks( α , β )
+    α ^2 - 2 * β
+end
+
+greeks(12.2,44.0)
+##
+
+f(x) = 2*x + 3
+
+println(f(4))
+##
+using Plots
+x = 1:10; y = rand(10); # These are the plotting data
+plot(x,y, label="my label")
+
+
+##
+
